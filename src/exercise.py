146f6647f0583e4ef12b62f4d25a0954a420acf0
Jake Lee <jake.lee@fdmgroup.com>
2021-09-28 21:31:11 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    nums = []
+    while True:
+        num = int(input())
+        if (num == -1):
+            break
+        else:
+            nums.append(num)
+    sum = 0
+    for number in nums:
+        sum += number
+    print("Sum: " + str(sum))
 
 if __name__ == '__main__':
     main()
